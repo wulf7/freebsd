020e21f6c4f38dc26c2c9c0f3708f5d054c02087
Vladimir Kondratiev <wulf@cicgroup.ru>
2016-06-23 23:02:16 +0300

    Add myself to authors :-)

--- sys/dev/evdev/cdev.c
+++ sys/dev/evdev/cdev.c
@@ -1,5 +1,6 @@
 /*-
  * Copyright (c) 2014 Jakub Wojciech Klama <jceel@FreeBSD.org>
+ * Copyright (c) 2015-2016 Vladimir Kondratyev <wulf@cicgroup.ru>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
